1e5b9f510e8ba1fed184e171a46c69d456ed2a73
swint29 <42974643+swint29@users.noreply.github.com>
2020-05-31 20:05:04 -0400

    small styling adjustments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-function techSwitch(){
-    document.getElementById("roles").style.color = 'black';
-    document.getElementById("technologies").style.color = 'red';
+function switchTab(){
+    
+    var tab1Color = document.getElementById("tab1").style.color;
+    var tab2Color = document.getElementById("tab2").style.color;
+    if(tab1Color=="rgb(170, 85, 85)"){
+
+        document.getElementById("tab1").style.color = "black";
+        document.getElementById("tab2").style.color = "rgb(170, 85, 85)";
+    }    
+    else{
+        document.getElementById("tab1").style.color = "rgb(170, 85, 85)";
+        document.getElementById("tab2").style.color = "black";
+    }
 };
